cb056b179c5ba1398e5f43ef8560b015a20d8cfe
AdamJHowell <adam.j.howell@gmail.com>
2023-02-10 21:00:28 -0700

    Trying to use a second sensor.

--- RPiDS18B20.py
+++ RPiDS18B20.py
@@ -10,10 +10,10 @@ base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob( base_dir + '28*' )[1]
 device_file = device_folder + '/w1_slave'
 device_list = []
-lines_list = []
 
 
 def read_lines_from_file():
+  lines_list = []
   print( f"Opening '{glob.glob( base_dir + '28*' )}'" )
   for device in glob.glob( base_dir + '28*' ):
     device_list.append( device + '/w1_slave' )
