ce62595265aac3e6b137d7a18e7d87c3280bdb95
AdamJHowell <adam.j.howell@gmail.com>
2023-02-14 10:17:46 -0700

    This should work dynamically. It will need some cleanup, but I'll wait until after it has been tested.

--- RPi_Multiple_DS18B20.py
+++ RPi_Multiple_DS18B20.py
@@ -10,26 +10,29 @@ base_dir = "/sys/bus/w1/devices/28*"
 device_folder_suffix = "/w1_slave"
 
 # Get all the filenames that begin with 28 in the path base_dir.
-device_file = glob.glob( base_dir )[0] + device_folder_suffix
-device_file1 = glob.glob( base_dir )[1] + device_folder_suffix
+# device_file = glob.glob( base_dir )[0] + device_folder_suffix
+# device_file1 = glob.glob( base_dir )[1] + device_folder_suffix
 
 
 def device_list_populate():
+  list_of_devices = []
   for index, item in enumerate( glob.glob( base_dir ) ):
     print( index, item )
+    list_of_devices.append( item + device_folder_suffix )
+  return list_of_devices
 
 
 def device_count():
-  device_list = glob.glob( base_dir )
-  for device in device_list:
-    print( f"  Device found at directory: {device}" )
-  return len( device_list )
+  list_of_devices = glob.glob( base_dir )
+  for discovered_device in list_of_devices:
+    print( f"  Device found at directory: {discovered_device}" )
+  return len( list_of_devices )
 
 
 # Read the temperature from each folder
 def read_temp_raw( file ):
-  with open( file, 'r' ) as device:
-    lines = device.readlines()
+  with open( file, 'r' ) as device_file:
+    lines = device_file.readlines()
     # print( f"read_temp_raw() lines: {lines}" )
     return lines
 
@@ -48,13 +51,14 @@ def read_temp( file_to_read ):
 
 if __name__ == "__main__":
   print( f"Welcome to {program_name}" )
-  device_list_populate()
+  device_list = device_list_populate()
   print( f"Detected {device_count()} devices." )
   try:
     while True:
       # Read the temperature data and print the value from each individual sensor.
-      print( ' C1=%3.3f  F1=%3.3f' % read_temp( device_file ) )
-      print( ' C2=%3.3f  F2=%3.3f' % read_temp( device_file1 ) )
+      for device in device_list:
+        print( ' C1=%3.3f  F1=%3.3f' % read_temp( device ) )
+      # print( ' C2=%3.3f  F2=%3.3f' % read_temp( device_file1 ) )
       time.sleep( 5 )
   except KeyboardInterrupt:
     print()
