204f2073f3dcd56bb8fb481112794e38b4b77029
AdamJHowell <adam.j.howell@gmail.com>
2023-02-14 10:00:07 -0700

    Removed most of the repeated code.

--- RPi_Multiple_DS18B20.py
+++ RPi_Multiple_DS18B20.py
@@ -2,7 +2,6 @@
 Source: https://www.hackster.io/vinayyn/multiple-ds18b20-temp-sensors-interfacing-with-raspberry-pi-d8a6b0
 """
 import glob
-import os
 import time
 
 
@@ -10,10 +9,6 @@ base_dir = '/sys/bus/w1/devices/'
 device_address_suffix = "28*"
 device_folder_suffix = "/w1_slave"
 device_folder_name = "/name"
-
-# These lines mount the device:
-# os.system( 'modprobe w1-gpio' )
-# os.system( 'modprobe w1-therm' )
 
 # Get all the filenames that begin with 28 in the path base_dir.
 device_folder = glob.glob( base_dir + device_address_suffix )[0]
@@ -26,44 +21,23 @@ device_file1 = device_folder1 + device_folder_suffix
 def device_count():
   device_list = glob.glob( base_dir + device_address_suffix )
   for device in device_list:
-    print( f"Device found: {device}" )
+    print( f"  Device found at directory: {device}" )
   return len( device_list )
 
 
-def read_rom():
-  name_file = device_folder + device_folder_name
-  f = open( name_file, 'r' )
-  # print('f:',f)
-  return f.readline()
-
-
-def read_rom1():
-  name_file1 = device_folder1 + device_folder_name
-  g = open( name_file1, 'r' )
-  # print('g:',g)
-  return g.readline()
-
-
 # Read the temperature from each folder
-def read_temp_raw():
-  with open( device_file, 'r' ) as device:
+def read_temp_raw( file ):
+  with open( file, 'r' ) as device:
     lines = device.readlines()
     # print( f"read_temp_raw() lines: {lines}" )
     return lines
 
 
-def read_temp_raw1():
-  with open( device_file1, 'r' ) as device1:
-    lines1 = device1.readlines()
-    # print( f"read_temp_raw()1 lines: {lines1}" )
-    return lines1
-
-
 # Convert the temperature data to a human-readable format.
-def read_temp():
-  lines = read_temp_raw()
+def read_temp( file_to_read ):
+  lines = read_temp_raw( file_to_read )
   while lines[0].strip()[-3:] != 'YES':
-    lines = read_temp_raw()
+    lines = read_temp_raw( file_to_read )
   equals_pos = lines[1].find( 't=' )
   temp_string = lines[1][equals_pos + 2:]
   temp_c = float( temp_string ) / 1000.0
@@ -71,24 +45,13 @@ def read_temp():
   return temp_c, temp_f
 
 
-def read_temp1():
-  lines1 = read_temp_raw1()
-  while lines1[0].strip()[-3:] != 'YES':
-    lines1 = read_temp_raw1()
-  equals_pos1 = lines1[1].find( 't=' )
-  temp_string1 = lines1[1][equals_pos1 + 2:]
-  temp_c1 = float( temp_string1 ) / 1000.0
-  temp_f1 = temp_c1 * 9.0 / 5.0 + 32.0
-  return temp_c1, temp_f1
-
-
 if __name__ == "__main__":
   print( f"Detected {device_count()} devices." )
   try:
     while True:
       # Read the temperature data and print the value from each individual sensor.
-      print( ' C1=%3.3f  F1=%3.3f' % read_temp() )
-      print( ' C2=%3.3f  F2=%3.3f' % read_temp1() )
+      print( ' C1=%3.3f  F1=%3.3f' % read_temp( device_file ) )
+      print( ' C2=%3.3f  F2=%3.3f' % read_temp( device_file1 ) )
       time.sleep( 5 )
   except KeyboardInterrupt:
     print()
